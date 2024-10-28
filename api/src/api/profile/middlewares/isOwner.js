1d6f9143fb3d4ce2a3254300b6d5cac1df9aad95
Napat6509650492 <napat.thai@dome.tu.ac.th>
2024-10-28 14:15:51 +0700

    edit isOwner middle

--- api/src/api/profile/middlewares/isOwner.js
+++ api/src/api/profile/middlewares/isOwner.js
@@ -7,26 +7,40 @@
 module.exports = (config, { strapi }) => {
   
   return async (ctx, next) => {
-    const user = ctx.state.user;
-    const entryId = await ctx.params.id ? ctx.params.id : undefined;
-    let entry = {};
-    
-    if(!(!isNaN(entryId) && !isNaN(parseFloat(entryId)))){
-      return ctx.badRequest("ID must be digit only")
-    }
+    try {
+      const user = ctx.state.user;
+      var entryId = await ctx.params.id ? ctx.params.id : undefined;
+      let entry = {};
+      
+      if (!user)
+        return ctx.unauthorized("User authentication is required.");
 
-    if (entryId) {
+      if (!entryId) {
+        return ctx.badRequest("Profild ID is required.")
+      }
+
+      if(!(!isNaN(entryId) && !isNaN(parseFloat(entryId))))
+        return ctx.badRequest("ID must be digit only")
+      
       entry = await strapi.entityService.findOne(
         'api::profile.profile',
         entryId,
         { populate: "*" }
       );
-    }
 
-    if (user.id !== entry.user.id) {
-      return ctx.unauthorized("This action is unauthorized.");
-    } else {
-      return next();
+      if (!entry) 
+        return ctx.notFound("Profile not found.");
+      
+      
+      if (user.id !== entry.user.id) {
+        return ctx.unauthorized("This action is unauthorized.");
+      } else {
+        return next();
+      }
+
+    } catch (error) {
+      strapi.log.error(error);
+      return ctx.internalServerError("An error occurred.");
     }
   };
 };
